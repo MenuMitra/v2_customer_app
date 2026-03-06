120292913a759095f26b21f3add71a376fcd7137
ABC <j.rushikesh013@gmail.com>
2026-03-06 14:17:49 +0530

    Changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "b078daa49e7326457d1803f1c16acef9"
   }, {
     "url": "index.html",
-    "revision": "0.6ir3cekb6tg"
+    "revision": "0.ic8f9bu7b9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
