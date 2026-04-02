f81cd93e5090557740a6d5b70eee4dd0bdf146ce
ABC <j.rushikesh013@gmail.com>
2026-04-02 13:11:44 +0530

    changes Done

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "b078daa49e7326457d1803f1c16acef9"
   }, {
     "url": "index.html",
-    "revision": "0.8ag1qb6g6s"
+    "revision": "0.kvl7t27d8b"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
